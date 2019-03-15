2009228a29cf3ac74925adb27f5d19c51337c810
pondelion <programming.deve@gmail.com>
2019-03-16 00:57:18 +0900

    update LoggingTile
    Change-Id: Iacaace0b63dd65f65f78e99d1e226fdb8db30051

--- packages/SystemUI/src/com/android/systemui/qs/tiles/LoggingTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/LoggingTile.java
@@ -3,6 +3,9 @@ package com.android.systemui.qs.tiles;
 import android.app.ActivityManager;
 import android.content.Intent;
 import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
 
 import com.android.systemui.R;
 import com.android.systemui.qs.QSHost;
@@ -12,6 +15,7 @@ import com.android.systemui.qs.tileimpl.QSTileImpl;
 /** Quick settings tile: Logging **/
 public class LoggingTile extends QSTileImpl<BooleanState> {
 
+    public static final String TAG = "LoggingTile";
     public static final String LOGGING_SETTING = Settings.System.LOGGING_MODE;
 
     public LoggingTile(QSHost host) {
@@ -25,7 +29,14 @@ public class LoggingTile extends QSTileImpl<BooleanState> {
 
     @Override
     protected void handleClick() {
-        
+        try {
+            boolean isEnabled = isLoggingModeEnabled();
+            Settings.System.putIntForUser(mContext.getContentResolver(), LOGGING_SETTING,
+                    isEnabled ? 0 : 1, ActivityManager.getCurrentUser());
+        } catch (final SettingNotFoundException e) {
+            Log.w(TAG, "Settings [" + LOGGING_SETTING + "] not found.");
+        }
+        refreshState();
     }
 
     @Override
@@ -35,9 +46,17 @@ public class LoggingTile extends QSTileImpl<BooleanState> {
 
     @Override
     protected void handleUpdateState(BooleanState state, Object arg) {
-        Settings.System.putIntForUser(mContext.getContentResolver(), LOGGING_SETTING,
-                state.value ? Settings.System.LOGGING_MODE_ON : Settings.System.LOGGING_MODE_OFF,
-                ActivityManager.getCurrentUser());
+        try {
+            boolean isEnabled = isLoggingModeEnabled();
+            final Drawable mEnable = mContext.getDrawable(R.drawable.sun);
+            final Drawable mDisable = mContext.getDrawable(R.drawable.sun2);
+            state.value = isEnabled;
+            state.icon = new DrawableIcon(state.value ? mEnable : mDisable);
+        } catch (final SettingNotFoundException e) {
+            Log.w(TAG, "Settings [" + LOGGING_SETTING + "] not found.");
+        }
+        state.label = "logging mode";
+        state.contentDescription = state.label;
     }
 
     @Override
@@ -53,6 +72,11 @@ public class LoggingTile extends QSTileImpl<BooleanState> {
     @Override
     public int getMetricsCategory() {
         return 0;
+    }
+
+    private boolean isLoggingModeEnabled() throws SettingNotFoundException {
+        return Settings.System.getIntForUser(mContext.getContentResolver(), LOGGING_SETTING,
+                ActivityManager.getCurrentUser()) == 1 ? true : false;
     }
 
 }
